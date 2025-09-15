a88ecbae2cf8445818d69d66e3114558dde68c24
Gr8rPotater <miles.jamesson@gmail.com>
2025-09-14 21:06:42 -0700

    This is supposed to be commiting

---
+++ Assets/TutorialInfo/Scripts/HelloWorld.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class HelloWorld : MonoBehaviour
+{
+
+
+        void Update()
+{
+        
+    }
+    void Start()
+    {
+        Debug.Log("Hello World");
+        Debug.Log("The world stays silent in response");
+
+
+    }
+
+    }
+
+
